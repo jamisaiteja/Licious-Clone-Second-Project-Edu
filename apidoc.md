48a9a97efa289dbf4f08f7cdd44108af610ac70c
jamisaiteja <jamisaiteja666@gmail.com>
2023-01-22 13:12:13 +0530

    commit

---
+++ apidoc.md
@@ -0,0 +1,30 @@
+# page 1
+
+>list of categories
+http://localhost:9310/categories
+>list of Best Sellers
+http://localhost:9310/bestsellers
+>list of Boneless Cuts
+http://localhost:9310/bonelesscuts
+
+# page 2
+
+> Items wrt to category
+http://localhost:9310/detail/11
+> Items wrt to category and Category Type
+
+# page 3
+> Details of the item
+
+
+# page 4
+
+>Item Details
+>Place Order
+
+# page 5
+
+>List of Orders
+>Order wrt to email
+>Update payment details
+>Delete orders
--- restapi/app.js
+++ restapi/app.js
@@ -1,18 +1,71 @@
 let express = require('express');
 let app =express();
 let port= 9310;
+let cors = require('cors');
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+let mongoUrl = "mongodb://localhost:27017";
+let db;
+app.use(cors());
+
 
 
 app.get('/', function(req,res){
     res.send("Hi from Express")
 })
 
-app.get('/location', function(req,res){
-    res.send("Hi from Location")
+//get categories
+app.get('/categories', function(req,res){
+    db.collection('category').find().toArray((err,data)=>{
+        if(err) throw err;
+        res.send(data)
+    })
 })
 
-app.listen(port, (err)=>{
-    if(err) throw err,
-    console.log(`server listening on port ${port}`)
+// get Details 
+app.get('/details', function(req,res){
+    db.collection('Details').find().toArray((err,data)=>{
+        if(err) throw err;
+        res.send(data)
+    })
 })
+
+//get Best Sellers
+app.get('/bestsellers', function(req,res){
+    db.collection('Bestsellers').find().toArray((err,data)=>{
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+// get Boneless Cuts
+app.get('/bonelesscuts', function(req,res){
+    db.collection('Bonelesscuts').find().toArray((err,data)=>{
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+// get items wrt to category
+app.get('/detail/:id', function(req,res){
+    console.log(req.params.id) 
+    let category_id = Number(req.params.id)
+    db.collection('Details').find({category_id:category_id}).toArray((err,data)=>{
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+//connection with db
+MongoClient.connect(mongoUrl,(err,client)=>{
+    if(err) console.log("Error while Connecting")
+    db = client.db('liciousdata') //database name
+    app.listen(port, (err)=>{
+        if(err) throw err,
+        console.log(`server listening on port ${port}`)
+    })
+})
+
+
+
 
